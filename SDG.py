10c9448b7472b2323ef3998dc381a6bccdc73c8a
al-932 <al.mergez@gmail.com>
2022-07-21 16:33:41 +0200

    v0.2.6
    added addDrift method to add drift to a metaObject.
    added addOrganoid to generate an organoid like object.

--- SDG.py
+++ SDG.py
@@ -4,23 +4,24 @@ SDG (Synthetic Data Generator) class allows you to create data to try your
 scripts.
 
 @author: Alex-932
-@version : 0.2.5
+@version : 0.2.6
 """
 
 import numpy as np
 import pandas as pd
-# from random import randrange
 import matplotlib.pyplot as plt
 import tifffile
+from scipy.ndimage.filters import gaussian_filter
 
 class SDG():
     
     def __init__(self):
         "SDG allows you to create synthetic data through several methods."
         self.data = pd.DataFrame(columns = ["x", "y", "z", "tp", "track", 
-                                            "objID", "target"], 
+                                            "objID", "target", "metaObjID"], 
                                  dtype = "float")
         self.objectID = 0
+        self.metaObjID = 0
         self.track = 0
         self.objectTable = pd.DataFrame(columns = ["tp", "type", 
                                                    "origin", "radius"],
@@ -91,7 +92,8 @@ class SDG():
         Parameters
         ----------
         tp : int, optional
-            Time point (or frame) the sphere will be added in. The default is 0.
+            Time point (or frame) the sphere will be added in. The default is 
+            0.
         origin : list, optional
             Coordinates of the center of the sphere. The default is [0, 0, 0].
             Format : [X, Y, Z]
@@ -132,21 +134,22 @@ class SDG():
             x = np.cos(theta)*rho
             z = np.sin(theta)*rho
             
-            points.append([x, y, z, tp, track, self.objectID])
+            points.append([x, y, z, tp, track, self.objectID, self.metaObjID])
             track += 1
             
         points = pd.DataFrame(points, columns = ["x", "y", "z", "tp", "track",
-                                                 "objID"], 
+                                                 "objID", "metaObjID"], 
                                       dtype = "float")
         
         # Shifting the coordinates to get the center on the origin position.
-        points["x"] = (points["x"]+origin[0])*radius
-        points["y"] = (points["y"]+origin[1])*radius
-        points["z"] = (points["z"]+origin[2])*radius
+        points["x"] = points["x"]*radius+origin[0]
+        points["y"] = points["y"]*radius+origin[1]
+        points["z"] = points["z"]*radius+origin[2]
         
         # Saving and updating the different object variables.
         self.data = pd.concat([self.data, points], ignore_index = True)
         self.objectID += 1
+        self.metaObjID += 1
         self.track = max([track+1, self.track])
                 
     def addRotatingSphere(self, origin = [0, 0, 0], radius = 10, nframe = 20, 
@@ -185,6 +188,7 @@ class SDG():
         
         # Saving the first track ID and creating the first sphere.
         track = self.track
+        metaObjID = self.metaObjID
         self.addSphere(tp, origin, radius, track, sample)
         
         # Converting degree to radian
@@ -214,6 +218,11 @@ class SDG():
                 # Getting the cartesian coordinates.
                 cvalues = self.data.loc[point, ["x", "y", "z"]].copy()
                 
+                # Retrieving the offset from the centroid to [0, 0, 0].
+                cvalues["x"] -= origin[0]
+                cvalues["y"] -= origin[1]
+                cvalues["z"] -= origin[2]
+                
                 # Applying X rotation
                 tempy = cvalues["y"].copy()
                 cvalues["y"] = cvalues["y"]*np.cos(theta_x)-\
@@ -234,26 +243,176 @@ class SDG():
                     cvalues["y"]*np.sin(theta_z)
                 cvalues["y"] = tempx*np.sin(theta_z)+\
                     cvalues["y"]*np.cos(theta_z)
+                    
+                # Readding the offset from the centroid to [0, 0, 0].
+                cvalues["x"] += origin[0]
+                cvalues["y"] += origin[1]
+                cvalues["z"] += origin[2]
                 
                 # Converting them back and saving them in the data dataframe.
                 self.data.loc[point+sample] = list(cvalues)+\
-                    [frame, itrack, objID, np.nan]
+                    [frame, itrack, objID, np.nan, metaObjID]
                 
                 itrack += 1
             
             self.objectID += 1
-                
-    def fakeTif(self, savepath):
-        TP = self.data["tp"].value_counts(ascending = True).index
-        array = np.zeros((int(self.data["x"].max()+5),
-                          int(self.data["y"].max()+5),
-                          int(self.data["z"].max()+5)))
-        for tp in TP:
-            filename = "synth_"+str(int(tp))+".tif"
-            tifffile.imwrite(savepath+"\\"+filename, 
-                             array, metadata={'axes': 'ZYX'})
+            
+    def addDrift(self, metaObjID, drift = [1, 1, 1], randomized = False, 
+                 variation = 10):
+        """
+        Add drift to a moving object like 
+
+        Parameters
+        ----------
+        metaObjID : int
+            ID of the metaObject (rotating sphere for example).
+        drift : list, optional
+            Drift values : [dX, dY, dZ]. The default is [1, 1, 1].
+        randomized : bool, optional
+            If True, Drift values are randomized using variation value. 
+            Default is False.
+        variation : float, optional
+            Range of variation : {variation}*[-1,1]+Value. The default is 10.
+
+        """
+        # Subsampling the dataset and getting the objectIDs from it.
+        subdata = self.data[self.data["metaObjID"] == metaObjID].copy()
+        objectIDs = subdata["objID"].unique().tolist()
         
-    def showData(self, TP = "all", tracks = "all", limits = None):
+        # List to save the total displacement.
+        driftSum = [0, 0, 0]
+        
+        # Iterating over objectIDs.
+        for objID in objectIDs:
+            
+            # Creating a temporary list to prevent modification of the drift 
+            # values
+            tempDrift = drift.copy()
+            
+            if randomized :
+                for i in range(len(drift)):
+                    # Adding the variation*[1;-1] to the drift.
+                    tempDrift[i] += (variation*np.random.sample()-\
+                                     variation*np.random.sample())
+                    
+                    driftSum[i] += tempDrift[i]
+                    
+            else :
+                for i in range(len(drift)):
+                    driftSum[i] += tempDrift[i]
+            
+            # Adding the sum of the total displacement to all points from a 
+            # given object.
+            for ID in subdata[subdata["objID"] == objID].index:
+                self.data.loc[ID, "x"] += driftSum[0]
+                self.data.loc[ID, "y"] += driftSum[1]
+                self.data.loc[ID, "z"] += driftSum[2]          
+        
+    def addBlob(self, tp, origin = [0, 0, 0], radius = 5, nframe = 20, 
+                sample = 20):
+        pass
+            
+    def addVariability(self, metaObjID, variation = 1):
+        """
+        Modify the coordinates of the points belonging to the given object
+
+        Parameters
+        ----------
+        metaObjID : int
+            ID of the object.
+        variation : float, optional
+            Range of variation : {variation}*[-1,1]. The default is 1.
+
+        """
+        IDs = self.data[self.data["metaObjID"] == metaObjID].index
+        
+        for ID in IDs:
+            for axis in ["x", "y", "z"]:
+                self.data.loc[ID, axis] += (variation*np.random.sample()-\
+                                            variation*np.random.sample())
+                    
+    def addOrganoid(self, sample = 200, origin = [100, 100, 100], radius = 15, 
+                    nframe = 20, tp = 0, rotation = [15, 10, 5],
+                    drift = [1, 1, 1], randomizedDrift = True, 
+                    driftVariation = 1, variability = 1):
+        """
+        Create an organoid like object using existing methods.
+
+        Parameters
+        ----------
+        sample : int, optional
+            Number of points in the sphere. The default is 200.
+        origin : list, optional
+            Coordinates of the center of the sphere. The default is [0, 0, 0].
+            Format : [X, Y, Z]
+        radius : float, optional
+            Radius of the sphere. The default is 15.
+        nframe : int, optional
+            Number of frames in which the sphere is. The default is 20.
+        tp : int, optional
+            Starting time point. The default is 0.
+        rotation : list of float, optional
+            Rotation angle (°) for each axis. The default is [15, 10, 5].
+        drift : list, optional
+            Drift values : [dX, dY, dZ]. The default is [1, 1, 1].
+        randomizedDrift : bool, optional
+            If True, Drift values are randomized using variation value. 
+            Default is True.
+        driftVariation : float, optional
+            Range of variation : {variation}*[-1,1]+Value. The default is 1.
+        variability : float, optional
+            Range of variation : {variation}*[-1,1]. The default is 1.
+
+        """
+        # Getting the metaObjectID.
+        metaObjID = self.metaObjID
+        
+        # Adding the rotating sphere.
+        self.addRotatingSphere(origin, radius, nframe, sample, tp, rotation[0],
+                               rotation[1], rotation[2])
+        
+        # Adding drift to the sphere.
+        self.addDrift(metaObjID, drift, randomizedDrift, driftVariation)
+        
+        # Adding some noise to the points coordinates.
+        self.addVariability(metaObjID, variability)
+        
+                
+    def exportToTifs(self, savepath, gaussianSigma = 1, mode = "single"):
+        TP = self.data["tp"].value_counts(ascending = True).index
+        
+        if mode == "stacked":
+            stackedArray = []
+        
+        for tp in TP:
+            array = np.zeros((int(self.data["z"].max()+10),
+                              int(self.data["y"].max()+10),
+                              int(self.data["x"].max()+10)))
+            
+            filename = "synth_"+str(int(tp))+".tif"
+            
+            subdata = self.data[self.data["tp"] == tp].copy()
+            
+            array[subdata["z"].astype(int),
+                  subdata["y"].astype(int), 
+                  subdata["x"].astype(int)] = 10
+            
+            blurredArray = gaussian_filter(array, sigma = gaussianSigma)
+            
+            if mode == "single":
+                tifffile.imwrite(savepath+'\\'+filename, 
+                                 blurredArray.astype("float32"), imagej = True)
+            
+            elif mode == "stacked":
+                stackedArray.append(blurredArray)
+
+        if mode == "stacked":
+            tifffile.imwrite(savepath+"\\synth_stacked.tif", 
+                             np.array(stackedArray).astype("float32"), 
+                             imagej = True, 
+                             metadata={'axes': 'TZYX'})
+        
+    def showData(self, TP = "all", tracks = "all", limits = True, lim = None):
         """
         Show the path that the given track(s) are taking in 3D.
 
@@ -268,7 +427,6 @@ class SDG():
             Format : [x_min, x_max, y_min, y_max, z_min, z_max]
 
         """
-        
         # Creating the figure and the axes.
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
@@ -288,14 +446,14 @@ class SDG():
             tracks = [tracks]
         
         # Setting the limits of the figure.
-        if limits == None:
+        if lim is None:
             lim = pd.Series([self.data["x"].min()-5, self.data["x"].max()+5,
                              self.data["y"].min()-5, self.data["y"].max()+5,
                              self.data["z"].min()-5, self.data["z"].max()+5],
                             index = ["xm", "xM", "ym", "yM", "zm", "zM"],
                             dtype = "float")
         else :
-            lim = pd.Series(limits, 
+            lim = pd.Series(lim, 
                             index = ["xm", "xM", "ym", "yM", "zm", "zM"],
                             dtype = "float")
         
@@ -314,10 +472,11 @@ class SDG():
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
         
-        # Setting the axis' limits. 
-        ax.set_xlim3d([lim["xm"], lim["xM"]])
-        ax.set_ylim3d([lim["ym"], lim["yM"]])
-        ax.set_zlim3d([lim["zm"], lim["zM"]])
+        if limits :
+            # Setting the axis' limits. 
+            ax.set_xlim3d([lim["xm"], lim["xM"]])
+            ax.set_ylim3d([lim["ym"], lim["yM"]])
+            ax.set_zlim3d([lim["zm"], lim["zM"]])
         
         plt.show()
         plt.close()
@@ -391,13 +550,19 @@ class SDG():
 
         """
         if OAT :
+            tracksDir = savepath+'\\data\\tracks\\'
+            tifDir = savepath+'\\data\\organoid_images\\'
+            
             data = self.data.copy()
             data["ID"] = data.index
+            
             emptyRows = pd.DataFrame([[-1]*len(data.columns) 
                                       for row in range(3)],
                                      columns = data.columns,
                                      dtype = "int")
+            
             data = pd.concat([emptyRows, data])
+            
             data["FRAME"] = data["tp"]
             data["QUALITY"] = [0]*data.shape[0]
             data.rename(columns = {"track" : "TRACK_ID", "x": "POSITION_X", 
@@ -405,14 +570,18 @@ class SDG():
                                    "tp": "POSITION_T", 
                                    "target": "SPOT_TARGET_ID"}, 
                         inplace = True)
+            
             tracks = data.drop(columns = "SPOT_TARGET_ID")
-            tracks.to_csv(savepath+"\\tracks.csv")
+            tracks.to_csv(tracksDir+"tracks.csv")
+            
             data.rename(columns = {"ID": "SPOT_SOURCE_ID"}, inplace = True)
+            
             edges = data.loc[:,["SPOT_TARGET_ID", "SPOT_SOURCE_ID"]]
             edges.dropna(inplace = True)
-            edges.to_csv(savepath+"\\edges.csv")
-            return None
-        self.data.to_csv(savepath)
+            edges.to_csv(tracksDir+"edges.csv")
+            
+        else :
+            self.data.to_csv(savepath)
         
 if __name__ == "__main__":
     t = SDG()
